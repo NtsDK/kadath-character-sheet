8ec05ccaae5c74e983090a0bc4f2aaa7e25c5edb
Timofey Rechkalov <trechkalov@yandex.ru>
2026-03-06 01:57:07 +0500

    Перевел работу временного хранилища на dto

--- src/infrastructure/TempStorage.ts
+++ src/infrastructure/TempStorage.ts
@@ -1,4 +1,4 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { openDB, deleteDB, IDBPDatabase } from "idb";
 
 import { CharSheet } from "../domain/CharSheet";
@@ -8,14 +8,32 @@ import {
   getNewDefinedCharSheet,
 } from "../domainServices/charSheet";
 import { ITempStorage } from "../ports";
+import { IOC_IDS } from "../IoC/Symbols";
+import { NotificationStore } from "../domainServices/NotificationStore";
+
+import { chartSheetToDTO, dtoToCharSheet } from "./dtoUtils";
+import { CharSheetDTO } from "./types";
+import { validateCharSheetDTO } from "./validation";
 
 const DB_NAME = "KadathDB";
 const DB_VERSION = 1;
 const FILES_STORE = "files";
 
+type KadathDBSchema = {
+  [FILES_STORE]: {
+    key: IDBValidKey;
+    value: CharSheetDTO;
+  };
+};
+
 @injectable()
 export class TempStorage implements ITempStorage {
-  db!: IDBPDatabase;
+  db!: IDBPDatabase<KadathDBSchema>;
+
+  constructor(
+    @inject(IOC_IDS.NotificationStore)
+    public readonly notificationStore: NotificationStore,
+  ) {}
 
   async init(): Promise<void> {
     let initializeFilesStore = false;
@@ -36,17 +54,37 @@ export class TempStorage implements ITempStorage {
   }
 
   async getAllCharSheets(): Promise<CharSheet[]> {
-    return openDB(DB_NAME, DB_VERSION).then((db) => {
-      return db.getAll(FILES_STORE);
+    return openDB<KadathDBSchema>(DB_NAME, DB_VERSION).then(async (db) => {
+      const dtos = await db.getAll(FILES_STORE);
+      const charSheets: CharSheet[] = [];
+      for (const dto of dtos) {
+        if (validateCharSheetDTO(dto)) {
+          charSheets.push(dtoToCharSheet(dto));
+        } else {
+          // @ts-expect-error - ожидаем, что в dto есть имя
+          const name = dto?.name;
+          console.error(
+            `Ошибка загрузки листа персонажа ${name}`,
+            JSON.stringify(validateCharSheetDTO.errors, null, "  "),
+          );
+          this.notificationStore.notify({
+            type: "error",
+            message: "Ошибка загрузки из браузера",
+            description: `Ошибка загрузки листа персонажа ${name}`,
+            duration: 0,
+          });
+        }
+      }
+      return charSheets;
     });
   }
 
   async create(charSheet: CharSheet): Promise<void> {
-    await this.db.add(FILES_STORE, charSheet);
+    await this.db.add(FILES_STORE, chartSheetToDTO(charSheet));
   }
 
   async update(charSheet: CharSheet): Promise<void> {
-    await this.db.put(FILES_STORE, charSheet);
+    await this.db.put(FILES_STORE, chartSheetToDTO(charSheet));
   }
 
   async delete(id: string): Promise<void> {

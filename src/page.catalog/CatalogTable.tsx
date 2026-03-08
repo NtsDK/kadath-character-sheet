bfcf0fb2dccbf5dd5d2700123a15dfea5efb1f91
Timofey Rechkalov <trechkalov@yandex.ru>
2026-03-08 20:40:45 +0500

    Добавил экспорт и удаление выделенных листов персонажей

--- src/page.catalog/CatalogTable.tsx
+++ src/page.catalog/CatalogTable.tsx
@@ -1,9 +1,15 @@
-import { Table } from "antd";
+import { Button, Table } from "antd";
 import { observer } from "mobx-react-lite";
-import type { ColumnsType } from "antd/es/table";
+import type { ColumnsType, TableProps } from "antd/es/table";
+import { useState } from "react";
 
 import type { CharSheet } from "../domain/CharSheet";
-import { getCatalogPageUiStore } from "../IoC";
+import {
+  getCatalogPageUiStore,
+  getCharSheetStore,
+  getConfirmModalUiStore,
+  getExportManager,
+} from "../IoC";
 import { simpleDateFormat } from "../utils/simpleDateFormat";
 
 import { CharacterMenu } from "./CharacterMenu";
@@ -15,14 +21,14 @@ const columns: ColumnsType<CharSheet> = [
     dataIndex: "name",
     key: "name",
     sorter: (a, b) => a.name.localeCompare(b.name),
-    render: (_, record) => <CharacterLink charSheet={record} />
+    render: (_, record) => <CharacterLink charSheet={record} />,
   },
   {
     title: "Обновлено",
     dataIndex: "updatedAt",
     key: "updatedAt",
     sorter: (a, b) => a.updatedAt.getTime() - b.updatedAt.getTime(),
-    render: simpleDateFormat
+    render: simpleDateFormat,
   },
   {
     title: "",
@@ -34,6 +40,65 @@ const columns: ColumnsType<CharSheet> = [
 
 export const CatalogTable = observer(() => {
   const { charSheets } = getCatalogPageUiStore();
+  const exportManager = getExportManager();
+  const charSheetStore = getCharSheetStore();
 
-  return <Table<CharSheet> columns={columns} dataSource={charSheets} size="small"/>;
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const rowSelection: TableProps<CharSheet>["rowSelection"] = {
+    selectedRowKeys,
+    onChange: (selectedRowKeys: React.Key[]) => {
+      setSelectedRowKeys(selectedRowKeys);
+    },
+    getCheckboxProps: (record: CharSheet) => ({
+      name: record.id,
+    }),
+  };
+
+  return (
+    <>
+      <Table<CharSheet>
+        rowSelection={{ type: "checkbox", ...rowSelection }}
+        columns={columns}
+        dataSource={charSheets}
+        size="small"
+        rowKey={(el) => el.id}
+      />
+      {selectedRowKeys.length > 0 && (
+        <div className="tw-fixed tw-bg-blue-900 tw-left-0 tw-top-0 tw-w-full tw-p-2 tw-text-right">
+          <Button
+            className="tw-mr-4"
+            onClick={() => {
+              const charSheets: CharSheet[] = [];
+              for (const key of selectedRowKeys) {
+                const charSheet = charSheetStore.get(key as string);
+                if (charSheet) {
+                  charSheets.push(charSheet);
+                }
+              }
+              exportManager.export(charSheets);
+              setSelectedRowKeys([]);
+            }}
+          >
+            Скачать выбранное
+          </Button>
+          <Button
+            onClick={() => {
+              getConfirmModalUiStore().confirm(
+                `Вы уверены, что хотите удалить выбранных персонажей?`,
+                () => {
+                  for (const key of selectedRowKeys) {
+                    charSheetStore.delete(key as string);
+                    setSelectedRowKeys([]);
+                  }
+                },
+              );
+            }}
+          >
+            Удалить выбранное
+          </Button>
+        </div>
+      )}
+    </>
+  );
 });
